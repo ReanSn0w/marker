b50232b55ee012da3c5c8830fa545e1880ec2a1c
Дмитрий Папков <papkovda@me.com>
2022-11-24 19:04:42 +0300

    - Удален метод NewPage. Практика показала, что пользоваться им не имеет смысла. - Метод WritePage структуры Marker переименован во Write

--- pkg/marker/page.go
+++ pkg/marker/page.go
@@ -7,18 +7,6 @@ import (
 	"github.com/ReanSn0w/gew/pkg/view"
 )
 
-func NewPage(head, body view.View) *Marker {
-	return &Marker{
-		Content: view.Group(
-			view.External("<!DOCTYPE html>"),
-			Html(nil, view.Group(
-				Head(nil, head),
-				Body(nil, body),
-			)),
-		),
-	}
-}
-
 func NewMarker(content view.View) *Marker {
 	return &Marker{
 		Content: content,
@@ -29,7 +17,7 @@ type Marker struct {
 	Content view.View
 }
 
-func (p *Marker) WritePage(ctx context.Context, wr io.Writer) {
+func (p *Marker) Write(ctx context.Context, wr io.Writer) {
 	builder := &builder{}
 	ctx = withBuilder(ctx, builder)
 	builder.Build(ctx, wr, p.Content)
