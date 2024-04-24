3b7f1ac1d1ae4b6e38ef125436fff2d1d08f6466
Дмитрий Папков <reansnow@mac-mini--aceplace.local>
2024-04-24 16:39:48 +0300

    [Update] main.go
    - Обновлено превью

--- cmd/preview/main.go
+++ cmd/preview/main.go
@@ -5,227 +5,43 @@ import (
 
 	"github.com/ReanSn0w/gew/v3/pkg/view"
 	"github.com/ReanSn0w/marker/pkg/mk"
+	"github.com/ReanSn0w/marker/pkg/page"
 	"github.com/ReanSn0w/marker/pkg/tag"
 	"github.com/ReanSn0w/marker/pkg/ui"
 )
 
 func main() {
 	previewElement(
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"),
-				mk.Class("yellow"),
-			),
-			mk.Text("red"),
+		tag.Div(
+			view.For(20, func(i int) view.View {
+				return ui.Box(
+					tag.Img()(
+						mk.Set("src", "https://picsum.photos/300"),
+						mk.Set("alt", "image"),
+						ui.PlaceInBlock(),
+						mk.WithStyle().
+							Value("max-width", "100%").
+							Value("height", "auto").
+							Value("object-fit", "cover").
+							Extract(),
+					),
+					ui.Text("Hello, world!"),
+				)
+			}),
 		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
-		),
-		tag.Hr(),
-		ui.Grid(ui.NewGridPreference("20px", "20px", "1fr", "2fr", "3fr"))(
-			tag.Div(mk.Text("Blue"))(
-				ui.BackgroundColor("blue"),
-				ui.Height("30px"), mk.Class("blue"),
-			),
-			tag.Div(mk.Text("Green"))(
-				ui.BackgroundColor("green"),
-				ui.Height("60px"), mk.Class("green"),
-			),
-			tag.Div(mk.Text("Yellow"))(
-				ui.BackgroundColor("yellow"),
-				ui.Height("90px"), mk.Class("yellow"),
-			),
-			mk.Text("red"),
-		)(
-			ui.BackgroundColor("red"),
-			mk.Class("grid"),
+			ui.Grid().
+				Columns(ui.Fr(1), ui.Fr(1), ui.Fr(1)).
+				VGap(ui.Px(20)).
+				HGap(ui.Px(20)).
+				Extract(),
 		),
 	)
 }
 
 func previewElement(item ...view.View) {
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		mk.NewPage(mk.NewTag(false, "title")(mk.Text("Preview Page")))(item...).
+		page.New("Preview page").
+			Content(item...).
 			Write(r.Context(), http.StatusOK, w)
 	}))
 }
